8a166656de86bea6d3aee444909a190da71d275d
LCYforever <445995193@qq.com>
2020-08-24 20:25:39 +0800

    导出QWindow::fromWinId接口,增加QtXml模块的lqt文件

--- cpptoxml/parser/parser.cpp
+++ cpptoxml/parser/parser.cpp
@@ -639,8 +639,38 @@ bool Parser::parseUsing(DeclarationAST *&node)
   if (!parseName(ast->name))
     return false;
 
-  ADVANCE(';', ";");
+  // using xxx = xxx;
+  if (token_stream.lookAhead() == '=')
+  {
+	  CHECK('=');
+	  TypeSpecifierAST *spec = 0;
+	  if (!parseTypeSpecifierOrClassSpec(spec))
+	  {
+		  reportError(("Need a type specifier to declare"));
+		  return false;
+	  }
 
+	  token_stream.rewind((int)ast->name->start_token);
+	  const ListNode<InitDeclaratorAST*> *declarators = 0;
+	  if (!parseInitDeclaratorList(declarators))
+	  {
+		  //reportError(("Need an identifier to declare"));
+		  //return false;
+	  }
+	  token_stream.rewind((int)spec->end_token);
+	  ADVANCE(';', ";");
+
+	  TypedefAST *ast_ = CreateNode<TypedefAST>(_M_pool);
+	  ast_->type_specifier = spec;
+	  ast_->init_declarators = declarators;
+
+	  UPDATE_POS(ast_, start, token_stream.cursor());
+	  node = ast_;
+	  char* qs = Parser::tokenText(ast_).toLatin1().data();
+	  return true;
+  }
+
+  ADVANCE(';', ";");
   UPDATE_POS(ast, start, token_stream.cursor());
   node = ast;
 
@@ -1373,7 +1403,7 @@ bool Parser::parseEnumSpecifier(TypeSpecifierAST *&node)
 
   // skip enum class ident_name {: type};
   if (token_stream.lookAhead() == ':') {
-    if (token_stream.lookAhead(1) != Token_identifier) {
+    if (token_stream.lookAhead(1) != Token_identifier && token_stream.lookAhead(1) != Token_int) {
       token_stream.rewind((int) start);
       return false;
     }
